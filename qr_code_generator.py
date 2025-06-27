a52ae05af09c16a523fdba235892fcdb3d39d0eb
kenttyyy <kentajrins123@gmail.com>
2025-06-27 18:41:52 +0800

    colored qr

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -13,3 +13,16 @@ class QRCodeGenerator:
         out_path = Path(self.__filename).with_suffix(".png")
         img.save(out_path)
         return out_path
+
+# Colored Qr Code
+class StyledQRCode(QRCodeGenerator):
+
+    def __init__(
+        self,
+        content: str,
+        filename: str = "qr_styled.png",
+        fg_color: str = "#023047",
+        bg_color: str = "#ffffff",
+    ) -> None:
+        super().__init__(content, filename)
+        self.fg_color, self.bg_color = fg_color, bg_color

fad1305c10bba414a8375efe0b856b27374e9bcc
kenttyyy <kentajrins123@gmail.com>
2025-06-27 18:23:20 +0800

    import module

--- qr_code_generator.py
+++ qr_code_generator.py
@@ -1,8 +1,3 @@
 # Import the needed module
 import qrcode
 from pathlib import Path
-
-class QRCodeGenerator:
-    def __init__(self, content: str, filename: str = "qr.png") -> None:
-        self.__content = content
-        self.__filename = filename
